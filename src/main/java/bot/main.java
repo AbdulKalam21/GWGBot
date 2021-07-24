49ceebeda33863711e9ebd99a71c51518700c828
AKDev <siddiqueabdul21@gmail.com>
2021-07-24 15:06:57 +0530

    Updated the project structure

---
+++ src/main/java/bot/main.java
@@ -0,0 +1,29 @@
+package bot;
+
+import java.io.IOException;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import javax.security.auth.login.LoginException;
+
+public class main {
+
+  public static void main(String[] args) throws IOException {
+    HttpServer server = HttpServer.create(new InetSocketAddress(8500), 0);
+    HttpContext context = server.createContext("/");
+    context.setHandler(main::handleRequest);
+    server.start();
+    System.out.println("It works");
+  }
+
+  private static void handleRequest(HttpExchange exchange) throws IOException {
+    String response = "Games With Gabe Bot";
+    exchange.sendResponseHeaders(200, response.getBytes().length);//response code and length
+    OutputStream os = exchange.getResponseBody();
+    os.write(response.getBytes());
+    os.close();
+  }
+
+}
