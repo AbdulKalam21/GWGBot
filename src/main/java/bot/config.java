12e6ecc41dd828b327edd53721f330141be4db92
AKDev <siddiqueabdul21@gmail.com>
2021-07-24 21:01:35 +0530

    Removed the bot token

--- src/main/java/bot/config.java
+++ src/main/java/bot/config.java
@@ -2,7 +2,7 @@ package bot;
 
 public class config{
 
-    public static final String TOKEN = "ODY4MzczNTIyMzYyMjczODQz.YPut3Q.sqnjjATHmtkzJnPV0PMxKEPChFM";
+    public static final String TOKEN = "";
 
     public static final String PREFIX = "!";
 
