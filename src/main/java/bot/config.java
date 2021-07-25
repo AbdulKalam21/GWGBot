7a1578850fecec647818e3f92254e8cde30b895e
AKDev <siddiqueabdul21@gmail.com>
2021-07-25 14:19:37 +0530

    Removed the tokne

--- src/main/java/bot/config.java
+++ src/main/java/bot/config.java
@@ -2,7 +2,7 @@ package bot;
 
 public class config{
 
-    public static final String TOKEN = "ODY4MzczNTIyMzYyMjczODQz.YPut3Q.qDjJI84lISXi8oVTBz9ZMb3K_N0"; // Bot's Token
+    public static final String TOKEN = ""; // Bot's Token
 
     public static final String PREFIX = ".";                                                          // Bot's Prefix
 
