1b874f77c4e804ee5d30ff4aa11cba27039d30cb
AKDev21 <akdevbusiness@gmail.com>
2021-12-07 19:35:58 +0530

    fix: react with thunbs down instead of thumbs up

--- src/main/java/GWGBot/SFReaction.java
+++ src/main/java/GWGBot/SFReaction.java
@@ -5,8 +5,10 @@ import net.dv8tion.jda.api.entities.Message;
 public class SFReaction {
 	
 	public SFReaction(Message msg) {
-		msg.addReaction("U+1F44E").queue();
+
 		msg.addReaction("U+1F44D").queue();
+		msg.addReaction("U+1F44E").queue();
+		
 	}
 
 }
