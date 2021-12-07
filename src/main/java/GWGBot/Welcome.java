76fda189eff6812e43f6edfdded9f3ee06487154
AKDev21 <akdevbusiness@gmail.com>
2021-12-07 16:27:58 +0530

    fix: welcome system

--- src/main/java/GWGBot/Welcome.java
+++ src/main/java/GWGBot/Welcome.java
@@ -8,7 +8,7 @@ public class Welcome {
 	
 	public Welcome(TextChannel welcomeChannel, Member member) {
 		EmbedBuilder welcomeMessage = new EmbedBuilder();
-		welcomeMessage.setDescription("Welcome to " + welcomeChannel.getGuild().getName() + " " + "<@" + member.getId() + ">" + "\n\n");
+		welcomeMessage.setDescription("Welcome to " + welcomeChannel.getGuild().getName() + " `" + member.getUser().getName() + "`" + "\n\n");
 		welcomeMessage.appendDescription("- Read the rules <#" + Constants.RULES_CHANNEL_ID + ">" + "\n");
 		welcomeMessage.appendDescription("- Introduce yourself <#" + Constants.INTRODUCTION_CHANNEL_ID + ">" + "\n");
 		welcomeMessage.appendDescription("- Get some roles <#" + Constants.SELF_ROLES_CHANNEL_ID + ">" + "\n");
