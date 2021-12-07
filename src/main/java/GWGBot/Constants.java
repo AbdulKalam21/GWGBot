edcf34baaea845aa371dba91fc50efb6667fb9c9
AKDev21 <akdevbusiness@gmail.com>
2021-12-07 16:30:19 +0530

    removed bot toke

--- src/main/java/GWGBot/Constants.java
+++ src/main/java/GWGBot/Constants.java
@@ -2,16 +2,16 @@ package GWGBot;
 
 public class Constants {
 
-	public static final String BOT_TOKEN = "ODY4MzcwMDgxMDU3NDc2NjA4.YPuqqA.TEUNmPk2QbCoVvWgWrheKrwftxw";
+	public static final String BOT_TOKEN = "";
 	
 	public static String PREFIX = "!";
 	
-	public static final String WELCOME_CHANNEL_ID = "898664903139811328";
+	public static final String WELCOME_CHANNEL_ID = "";
 	
-	public static final String RULES_CHANNEL_ID = "786050022227705897";
+	public static final String RULES_CHANNEL_ID = "";
 	
-	public static final String INTRODUCTION_CHANNEL_ID = "786054666441064460";
+	public static final String INTRODUCTION_CHANNEL_ID = "";
 	
-	public static final String SELF_ROLES_CHANNEL_ID = "788617948112879616";
+	public static final String SELF_ROLES_CHANNEL_ID = "";
 
 }
