87e43a7911c99f4cfbfa1d34c833c5fd2c08dfd3
AbdulKalam21 <abdulkalam2185@gmail.com>
2022-02-10 17:08:39 +0530

    revert random embed color

--- src/Commands/About.ts
+++ src/Commands/About.ts
@@ -1,11 +1,13 @@
 import { Command, CommandContext, Embed } from "Harmony"
+import Constants from "Common/Constants.ts"
 
 class About extends Command {
     name = "about"
 
     async execute(ctx: CommandContext) {
+
         const embed = new Embed()
-//        .setColor(`#${Math.floor(Math.random()*16777215).toString(16)}`)
+        .setColor(Constants.RANDOM_EMBED_COLORS)
         .setTitle(`Hello i am ${ctx.client.user?.username}`)
         .setDescription("About Me, I am a general purpose bot written for\n Games With Gabe's official discord server")
         .addField("Github Repository:", "https://github.com/AbdulKalam21/GWGBot")
