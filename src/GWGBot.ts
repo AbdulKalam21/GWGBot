e52d0d82d24986e310b7d05aee1d851e3e867b57
AbdulKalam21 <abdulkalam2185@gmail.com>
2022-05-03 07:06:43 -0700

    add welcome event

--- src/GWGBot.ts
+++ src/GWGBot.ts
@@ -1,4 +1,4 @@
-import { CommandClient, Command, CommandContext, Client, Message, GatewayIntents, Member } from "Harmony"
+import { CommandClient, Command, CommandContext, Client, Message, GatewayIntents, Member, Embed } from "Harmony"
 import Constants from "Common/Constants.ts"
 import Ping from "Commands/Ping.ts"
 import About from "Commands/About.ts"
@@ -22,6 +22,34 @@ const GWGBot = new CommandClient({
 GWGBot.on("ready", () => {
     console.log(`Logged in as ${GWGBot.user?.tag}!`)
 })
+
+GWGBot.on("guildMemberAdd", async (member) => {
+	// TODO: Move this code to a separate file 
+    const guild = await GWGBot.guilds.get(Constants.GUILD_ID)
+
+    if (guild != undefined) {
+        const welcome = await guild.channels.get(Constants.WELCOME_CHANNEL_ID) 
+
+        if (welcome != undefined) {
+            if (welcome.isText()) {
+            const embed = new Embed()
+              .setColor(Constants.RANDOM_EMBED_COLORS)
+              .setTitle(`Welcome to ${guild.name} ${member.user.username}`)
+              .setThumbnail(member.user.avatarURL())
+              .setDescription(
+                `- Read the rules <#${Constants.RULES_CHANNEL_ID}>\n` +
+                `- Get some roles <#${Constants.SELF_ROLES_CHANNEL_ID}>\n` +
+                `- Introduce yourself <#${Constants.WELCOME_CHANNEL_ID}>`,
+              )
+              .setFooter(`You are our ${guild.memberCount}th member`, guild.iconURL());
+
+            await welcome.send({ embed: embed });
+            }
+
+        }
+
+    }
+})  
 
 GWGBot.commands.add(Ping)
 GWGBot.commands.add(About)
